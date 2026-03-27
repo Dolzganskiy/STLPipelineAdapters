642fb7cfcdd601c997499fe550428d9943c35385
Данил Лутцев <kepleomax@MacBook-Air-Danil.local>
2026-03-28 00:51:20 +0300

    Make dir

--- lib/sources/dir.h
+++ lib/sources/dir.h
@@ -1,1 +1,41 @@
 #pragma once
+#include <variant>
+
+
+class DirFlowIterator : public FlowRangeMixin<DirFlowIterator> {
+public:
+    using value_type = std::filesystem::path;
+
+    DirFlowIterator(std::string path, bool recursive) 
+        : recursive_(recursive) {
+        if (recursive) {
+            it_ = std::filesystem::recursive_directory_iterator(path);
+        } else {
+            it_ = std::filesystem::directory_iterator(path);
+        }
+    }
+
+    std::optional<value_type> Next() {
+        if (recursive_) {
+            auto& it = std::get<std::filesystem::recursive_directory_iterator>(it_);
+            if (it == std::filesystem::recursive_directory_iterator()) return std::nullopt;
+            auto result = it->path();
+            ++it;
+            return result;
+        }
+        auto& it = std::get<std::filesystem::directory_iterator>(it_);
+        if (it == std::filesystem::directory_iterator()) return std::nullopt;
+        auto result = it->path();
+        ++it;
+        return result;
+    }
+
+private:
+    bool recursive_;
+    std::variant<std::filesystem::directory_iterator, 
+        std::filesystem::recursive_directory_iterator> it_;
+};
+
+inline auto Dir(std::string path, bool recursive) {
+    return DirFlowIterator(std::move(path), recursive);
+}
